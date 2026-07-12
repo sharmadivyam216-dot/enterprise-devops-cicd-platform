34960f254448adb15436608ec77b29cd48db7c83
Divyam Sharma <sharmadivyam216@gmail.com>
2026-07-12 23:35:03 +0530

    Move Jenkinsfile to repository root

---
+++ Jenkinsfile
@@ -0,0 +1,13 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Hello DevOps') {
+            steps {
+                echo '🚀 Enterprise DevOps CI/CD Pipeline Started'
+            }
+        }
+
+    }
+}
---
+++ app/test_app.py
@@ -0,0 +1,12 @@
+from app import app
+
+client = app.test_client()
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json["status"] == "UP"
